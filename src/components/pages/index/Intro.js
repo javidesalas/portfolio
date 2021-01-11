30283207200ab73c46a2f5d2c32902a51aa4e497
Javier de Salas <chinostrike@gmail.com>
2021-01-11 19:57:05 +0100

    hero grid

--- src/components/pages/index/Intro.js
+++ src/components/pages/index/Intro.js
@@ -2,6 +2,9 @@ import React, { useContext, useState, useEffect}from 'react';
 import { LangContext } from '../../shared/LangContext'
 import { ThemeContext } from "../../shared/ThemeContext";
 
+import { ReactComponent as HeroJavi } from './hero-javi.svg'
+import { ReactComponent as HeroDe } from "./hero-de.svg";
+import { ReactComponent as HeroSalas } from "./hero-salas.svg";
 
 const Intro = ({
 				textHeroEn,
@@ -14,9 +17,12 @@ const Intro = ({
 	const { inEnglish }  = useContext(LangContext)
     
     return (
-			<section>
-				<div>
-					 <p>{inEnglish ? textHeroEn : textHeroEs}</p>
+			<section className="intro">
+				<div className="intro__hero">
+					<HeroJavi className="intro__hero__item intro__hero__item--javi" />
+					<HeroDe className="intro__hero__item intro__hero__item--de" />
+					<HeroSalas className="intro__hero__item intro__hero__item--salas" />
+					<p className="intro__hero__text">{inEnglish ? textHeroEn : textHeroEs}</p>
 				</div>
 				<p>{inEnglish ? descriptionEn : descriptionEs}</p>
 			</section>
