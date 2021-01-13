1c5786d9cfd6b17be89171e30238872105b73d0a
Javier de Salas <chinostrike@gmail.com>
2021-01-13 13:17:06 +0100

    experience phone

--- src/components/pages/index/Experience.js
+++ src/components/pages/index/Experience.js
@@ -2,35 +2,84 @@ import React, { useContext } from "react";
 import { LangContext } from "../../shared/LangContext";
 import { ThemeContext } from "../../shared/ThemeContext";
 
-
-
-
 const Experience = ({
-                    devEn,
-                    devEs,
-                    designEn,
-                    designEs,
-                    marketingEn,
-                    marketingEs,
-        }) => {
-    const { inEnglish } = useContext(LangContext);
-    
-    return (
-			<section>
-				<div>
-					<span>{inEnglish ? "Development" : "Desarrollo"}</span>
-					<span>Marketing</span>
-					<span>{inEnglish ? "Design" : "Diseño"}</span>
-				</div>
-				<div>
-					<article>{inEnglish ? devEn : devEs}</article>
-					<article>{inEnglish ? marketingEs : marketingEn}</article>
-					<article>{inEnglish ? designEn : designEs}</article>
-				</div>
-			</section>
-		);
+	devEn,
+	devEs,
+	designEn,
+	designEs,
+	marketingEn,
+	marketingEs,
+}) => {
+	const { inEnglish } = useContext(LangContext);
 
-	
+	const handleViewer = e => {
+		const headers = Array.of(...document.querySelectorAll(".experience__header__item"));
+		const container = document.querySelector(".experience__viewer__container")
+			const viewers = Array.of(...document.querySelectorAll(".experience__viewer__container__item"))
+		
+
+		headers.forEach((elm) => elm.classList.remove("experience__header__item--active"));
+		e.currentTarget.classList.add("experience__header__item--active");
+		
+		const transformation = `translateX(${parseInt(e.currentTarget.id) * -100}vw)`
+		container.style.setProperty("transform", transformation);
+		
+		viewers.forEach((elm) =>
+		elm.classList.add("experience__viewer__container__item--move")
+		);
+		viewers.forEach((elm) =>
+			elm.classList.remove("experience__viewer__container__item--move")
+		);
+	}
+
+	return (
+		<section className="experience">
+			<h2 className="experience__title">
+				{inEnglish ? "Experience" : "Experiencia"}
+			</h2>
+			<div className="experience__lg-container">
+				<div className="experience__header">
+					<h3
+						key="1"
+						id="0"
+						className="experience__header__item experience__header__item--dev"
+						onClick={handleViewer}
+					>
+						{inEnglish ? "Development" : "Desarrollo"}
+					</h3>
+					<h3
+						key="2"
+						id="1"
+						className="experience__header__item experience__header__item--marketing"
+						onClick={handleViewer}
+					>
+						Marketing
+					</h3>
+					<h3
+						key="3"
+						id="2"
+						className="experience__header__item experience__header__item--design"
+						onClick={handleViewer}
+					>
+						{inEnglish ? "Design" : "Diseño"}
+					</h3>
+				</div>
+				<div className="experience__viewer">
+					<div className="experience__viewer__container">
+						<article className="experience__viewer__container__item">
+							{inEnglish ? devEn : devEs}
+						</article>
+						<article className="experience__viewer__container__item">
+							{inEnglish ? marketingEs : marketingEn}
+						</article>
+						<article className="experience__viewer__container__item">
+							{inEnglish ? designEn : designEs}
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Experience;
--- src/components/pages/index/Intro.js
+++ src/components/pages/index/Intro.js
@@ -1,32 +1,32 @@
-import React, { useContext, useState, useEffect}from 'react';
-import { LangContext } from '../../shared/LangContext'
+import React, { useContext, useState, useEffect } from "react";
+import { LangContext } from "../../shared/LangContext";
 import { ThemeContext } from "../../shared/ThemeContext";
 
-import { ReactComponent as HeroJavi } from './hero-javi.svg'
+import { ReactComponent as HeroJavi } from "./hero-javi.svg";
 import { ReactComponent as HeroDe } from "./hero-de.svg";
 import { ReactComponent as HeroSalas } from "./hero-salas.svg";
 
-const Intro = ({
-				textHeroEn,
-				textHeroEs,
-				descriptionEn,
-				descriptionEs,
-        }) => {
-      
-   
-	const { inEnglish }  = useContext(LangContext)
-    
-    return (
-			<section className="intro">
-				<div className="intro__hero">
-					<HeroJavi className="intro__hero__item intro__hero__item--javi" />
-					<HeroDe className="intro__hero__item intro__hero__item--de" />
-					<HeroSalas className="intro__hero__item intro__hero__item--salas" />
-					<p className="intro__hero__text">{inEnglish ? textHeroEn : textHeroEs}</p>
-				</div>
-				<p>{inEnglish ? descriptionEn : descriptionEs}</p>
-			</section>
-		);
+const Intro = ({ textHeroEn, textHeroEs, descriptionEn, descriptionEs }) => {
+	const { inEnglish } = useContext(LangContext);
+
+	return (
+		<section className="intro">
+			<div className="intro__hero">
+				<HeroJavi className="intro__hero__item intro__hero__item--javi" />
+				<HeroDe className="intro__hero__item intro__hero__item--de" />
+				<HeroSalas className="intro__hero__item intro__hero__item--salas" />
+				<p className="intro__hero__text">
+					{inEnglish ? textHeroEn : textHeroEs}
+				</p>
+			</div>
+			<div className="intro__description">
+				<article className="intro__description__text">
+					{inEnglish ? descriptionEn : descriptionEs}
+				</article>
+				<div className="intro__description__background"></div>
+			</div>
+		</section>
+	);
 };
 
 export default Intro;
